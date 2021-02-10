4806c1ddb143e0a8bda11ab29f0cec65a68698f5
Joel Alfaro <alfarojoela@outlook.com>
2021-02-10 09:46:18 -0800

    math functions

---
+++ section21/javascript practive/app.js
@@ -0,0 +1,31 @@
+// console.log("HELLO");
+
+// if (1+ 1===2) {
+//     console.log("Math still works.")
+// }
+
+// let random =Math.random();
+// if (random < 0.5) {
+//     console.log ("your number is less than 0.5");
+//     console.log(random);
+// }
+
+// if(random >0.5){
+//     console.log("Your number is greater than 0.5.");
+//     console.log(random);
+// }
+
+// const password = prompt("please enter a new password");
+
+// if (password.length >= 6) {
+//     console.log("LONG ENOUGH PASSWORD!");
+// } else {
+//     console.log("password is too short")
+// }
+
+// if(password.indexOf(' ') === -1){
+//     console.log("Good job!  No space!")
+// } else {
+//   console.log("Password cannot contain spaces!");  
+// }
+
